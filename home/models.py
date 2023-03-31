f0719e652dda032db23bf39e2287f008f0ee038c
Shyam Sundar S <shyamss097@gmail.com>
2023-03-31 14:55:23 +0530

    added models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class waste(models.Model):
+    id=models.IntegerField(primary_key=True,max_length=25)
+    type=models.CharField(max_length=25)
+    quantity=models.CharField(max_length=25)
+    location=models.CharField(max_length=25)
+    current_status=models.CharField(max_length=25)
+
+
+
+class recycle(models.Model):
+    id=models.IntegerField(primary_key=True,max_length=25)
+    center_name=models.CharField(max_length=25)
+    location=models.CharField(max_length=25)
+    recyable_types=models.CharField(max_length=25)
+
+
+
+class treatment(models.Model):
+    id=models.IntegerField(primary_key=True,max_length=25)
+    name=models.CharField(max_length=25)
+    location=models.CharField(max_length=25)
+
+
+class user(models.Model):
+    id=models.IntegerField(primary_key=True,max_length=25)
+    name=models.CharField(max_length=25)
+    email=models.CharField(max_length=30)
+    
+
+class collection(models.Model):
+    id=models.IntegerField(primary_key=True,max_length=25)
+    collection_time=models.DateTimeField()
+    user_id=models.ForeignKey(user,default=None)
+
+
